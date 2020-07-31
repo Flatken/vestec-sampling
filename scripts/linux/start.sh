67f0af53a42b1b768721fbe2c9908b3c257abd91
Fellegara, Riccardo <riccardo.fellegara@dlr.de>
2020-07-31 16:14:32 +0200

    :beetle: fix linux compile and run scripts

--- scripts/linux/start.sh
+++ scripts/linux/start.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-module() { eval `/usr/bin/modulecmd bash $*`; }
-
-export MODULEPATH=/tools/modulesystem/modulefiles
-
-module purge
-module load iv-group
-module unload gdal
-
-module load python3/python-3.7.2/sled12.x86_64.gcc.release
 export LD_LIBRARY_PATH=../lib:$LD_LIBRARY_PATH
-export PYTHONPATH=$PYTHONPATH:../lib/python3.4/site-packages
-export PYTHONPATH=$PYTHONPATH:../bin:../lib
-
-echo $PYTHONPATH
-
-SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
-cd $SCRIPT_DIR
-
-mpiexec -np 2 ./VestecCatalystEmulator /unsecured/flat_ma/vestec/datasets/test/ 0.02 $1
+mpirun.mpich -np 1 ./VestecCatalystEmulator /unsecured/flat_ma/vestec/datasets/test/ .vtk 0.02 criticalPoints_Evaluation.py
 
