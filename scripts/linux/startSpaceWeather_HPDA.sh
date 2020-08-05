a28bd56340a92a130ac2cb55d495fb08606172d2
Fellegara <Riccardo.Fellegara@dlr.de>
2020-08-05 15:26:00 +0200

    :wrench: fix linux script for HPDA cluster

--- scripts/linux/startSpaceWeather_HPDA.sh
+++ scripts/linux/startSpaceWeather_HPDA.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export LD_LIBRARY_PATH=../lib:$LD_LIBRARY_PATH
-mpirun.mpich -np 1 ./VestecCatalystEmulator /scratch/VESTEC-DATASETS/space_weather/run007/ .vtk 0.02 criticalPoints_SpaceWeather.py
+srun -N $1 -c $2 --exclusive ./VestecCatalystEmulator /scratch/VESTEC-DATASETS/space_weather/run007/ .vtk 0.02 criticalPoints_SpaceWeather.py > $3
 
--- scripts/linux/start_HPDA.sh
+++ scripts/linux/start_HPDA.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export LD_LIBRARY_PATH=../lib:$LD_LIBRARY_PATH
-mpirun.mpich -np 1 ./VestecCatalystEmulator /scratch/VESTEC-DATASETS/2D/ .vtk 0.02 criticalPoints_Evaluation.py
+srun -N $1 -c $2 --exclusive ./VestecCatalystEmulator /scratch/VESTEC-DATASETS/2D/ .vtk 0.02 criticalPoints_Evaluation.py > $3
 
