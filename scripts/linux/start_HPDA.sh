2a871251e0412075e2d73bd814cf847a198a4c8d
Fellegara <Riccardo.Fellegara@dlr.de>
2020-08-06 12:48:12 +0200

    :wrench: update hpda scripts

--- scripts/linux/start_HPDA.sh
+++ scripts/linux/start_HPDA.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export LD_LIBRARY_PATH=../lib:$LD_LIBRARY_PATH
-#export OMP_NUM_THREADS=$3
-srun -N $1 -n $2 -c $3 --exclusive ./VestecCatalystEmulator /scratch/VESTEC-DATASETS/2D/ .vtk 0.02 criticalPoints_Evaluation.py > criticalPoints_2DFlow_$1_$2_$3.csv
+export OMP_NUM_THREADS=$3
+srun -N $1 -n $2 -c 56 --exclusive ./VestecCatalystEmulator /scratch/VESTEC-DATASETS/2D/ .vtk 0.02 criticalPoints_Evaluation.py > criticalPoints_2DFlow_$1_$2_$3.csv
 
