1bac4c4869016805638729ce4df54c41db07286e
Fellegara <Riccardo.Fellegara@dlr.de>
2020-08-06 09:17:14 +0200

    :wrench: update hpda scripts

--- scripts/linux/startSpaceWeather_HPDA.sh
+++ scripts/linux/startSpaceWeather_HPDA.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export LD_LIBRARY_PATH=../lib:$LD_LIBRARY_PATH
-export OMP_NUM_THREADS=$3
-srun -N $1 -n $2 -c $3 --exclusive ./VestecCatalystEmulator /scratch/VESTEC-DATASETS/space_weather/run007/ .vtk 0.02 criticalPoints_SpaceWeather.py
+#export OMP_NUM_THREADS=$3
+srun -N $1 -n $2 -c $3 --exclusive ./VestecCatalystEmulator /scratch/VESTEC-DATASETS/space_weather/run007/ .vtk 0.02 criticalPoints_SpaceWeather.py > criticalPoints_spaceWeather_$1_$2_$3.csv
 
 
