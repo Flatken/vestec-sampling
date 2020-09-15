62aa66b58a829c13efd22081e1f891fe2b443f89
Flatken <flat_ma@fe-store01.sc.bs.dlr.de>
2020-09-15 15:27:22 +0200

    OpenMP environment opimization

--- scripts/linux/startSpaceWeather_HPDA.sh
+++ scripts/linux/startSpaceWeather_HPDA.sh
@@ -2,6 +2,11 @@
 
 export LD_LIBRARY_PATH=../lib:$LD_LIBRARY_PATH
 export OMP_NUM_THREADS=$3
-srun -N $1 -n $2 -c 56 --exclusive ./VestecCatalystEmulator /scratch/VESTEC-DATASETS/space_weather/run007/ .vtk 0.02 criticalPoints_SpaceWeather.py > criticalPoints_spaceWeather_$1_$2_$3.csv
+export OMP_DISPLAY_ENV=true
+export OMP_PROC_BIND=spread #close,master,spread
+export OMP_PLACES=cores #socket,cores,threads
+
+srun -N $1 -n $2 -c 56 --exclusive ./VestecCatalystEmulator /scratch/VESTEC-DATASETS/space_weather/run007/ .vtk 0.02 criticalPoints_SpaceWeather.py 
+#> criticalPoints_spaceWeather_$1_$2_$3.csv
 
 
