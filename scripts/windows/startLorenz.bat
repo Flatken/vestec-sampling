33b68ad8496d4a69e9db82a48495047e484b9b2b
Fellegara <Riccardo.Fellegara@dlr.de>
2020-09-17 09:49:38 +0200

    :wrench: Update Lorenz and SpaceWeather bat scripts

--- scripts/windows/startLorenz.bat
+++ scripts/windows/startLorenz.bat
@@ -2,6 +2,12 @@
 set PATH=../lib:%PATH%
 set PYTHONPATH=%PYTHONPATH%:Lib/site-packages
 set PYTHONPATH=%PYTHONPATH%:../bin:../lib
+
+set OMP_NUM_THREADS=%2
+set OMP_DISPLAY_ENV=true
+set OMP_PROC_BIND=spread #close,master,spread
+set OMP_PLACES=cores #socket,cores,threads
+
 start smpd -d 3
-mpiexec -hosts 1 localhost -cores 4 VestecCatalystEmulator.exe "D:\\vr_data\\LorenzAttractor\\" .vtk 1 criticalPoints_Lorenz.py
+mpiexec -hosts 1 localhost -cores %1 VestecCatalystEmulator.exe "C:\VESTEC\Datasets\lorenz" .vtk 1 criticalPoints_Lorenz.py
 @echo on
