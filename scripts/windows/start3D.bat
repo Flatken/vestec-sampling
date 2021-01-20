55945d645979f600578d34947fc2935ec6778f47
Fellegara <Riccardo.Fellegara@dlr.de>
2021-01-20 17:09:47 +0100

    :wrench: updated bat scripts + added python script for Taylor green dataset

--- scripts/windows/start3D.bat
+++ scripts/windows/start3D.bat
@@ -2,5 +2,5 @@
 set PV_PLUGIN_PATH=.;paraview-5.8/plugins/VestecPlugins
 
 start smpd -d 3
-mpiexec -env PV_PLUGIN_PATH %PV_PLUGIN_PATH% -env OMP_NUM_THREADS %2 -env OMP_DISPLAY_ENV true -env OMP_PROC_BIND spread -env OMP_PLACES cores -hosts 1 localhost -cores 4 VestecCatalystEmulator.exe "C:\\VESTEC\\Datasets\\taylor_green" .vtk 1 criticalPoints_Evaluation.py
+mpiexec -env PV_PLUGIN_PATH %PV_PLUGIN_PATH% -env OMP_NUM_THREADS %2 -env OMP_DISPLAY_ENV true -env OMP_PROC_BIND spread -env OMP_PLACES cores -hosts 1 localhost -cores %1 VestecCatalystEmulator.exe "C:\\VESTEC\\Datasets\\taylor_green" .vtk 1 scripts\criticalPoints_TaylorGreen.py
 @echo on
--- scripts/windows/startSpaceWeather.bat
+++ scripts/windows/startSpaceWeather.bat
@@ -2,5 +2,5 @@
 set PV_PLUGIN_PATH=.;paraview-5.8/plugins/VestecPlugins
 
 start smpd -d 3
-mpiexec -env PV_PLUGIN_PATH %PV_PLUGIN_PATH% -env OMP_NUM_THREADS %2 -env OMP_DISPLAY_ENV true -env OMP_PROC_BIND spread -env OMP_PLACES cores -hosts 1 localhost -cores %1 VestecCatalystEmulator.exe "D:\\vr_data\\VESTEC\\space_weather\\run007" .vtk 40 scripts/criticalPoints_SpaceWeather.py
+mpiexec -env PV_PLUGIN_PATH %PV_PLUGIN_PATH% -env OMP_NUM_THREADS %2 -env OMP_DISPLAY_ENV true -env OMP_PROC_BIND spread -env OMP_PLACES cores -hosts 1 localhost -cores %1 VestecCatalystEmulator.exe "C:\VESTEC\Datasets\space_weather\run007" .vtk 40 scripts/criticalPoints_SpaceWeather.py
 @echo on
