ddd68a90870914d4f37f9df69c95d85e655e1f29
Fellegara <fell_ri@fe-store01.sc.bs.dlr.de>
2021-02-01 13:22:43 +0100

    :wrench: update HPDA scripts

--- scripts/linux/startLorenz_HPDA.sh
+++ scripts/linux/startLorenz_HPDA.sh
@@ -4,6 +4,10 @@ export LD_LIBRARY_PATH=../lib:$LD_LIBRARY_PATH
 export PV_PLUGIN_PATH=../lib/paraview-5.8/plugins/VestecPlugins
 
 export OMP_NUM_THREADS=$3
+export OMP_DISPLAY_ENV=true
+export OMP_PROC_BIND=spread #close,master,spread
+export OMP_PLACES=cores #socket,cores,threads
+
 srun -N $1 -n $2 -c 56 --exclusive ./VestecCatalystEmulator /scratch/VESTEC-DATASETS/lorenz/ .vtk 0.02 $PWD/scripts/criticalPoints_Lorenz.py > criticalPoints_Lorenz_$1_$2_$3.csv
 
 
--- scripts/linux/startTaylorGreen_HPDA.sh
+++ scripts/linux/startTaylorGreen_HPDA.sh
@@ -8,6 +8,6 @@ export OMP_DISPLAY_ENV=true
 export OMP_PROC_BIND=spread #close,master,spread
 export OMP_PLACES=cores #socket,cores,threads
 
-srun -N $1 -n $2 -c 56 --exclusive ./VestecCatalystEmulator /scratch/VESTEC-DATASETS/space_weather/run007/ .vtk 0.02 $PWD/scripts/criticalPoints_SpaceWeather.py > criticalPoints_spaceWeather_$1_$2_$3.csv
+srun -N $1 -n $2 -c 56 --exclusive ./VestecCatalystEmulator /scratch/VESTEC-DATASETS/taylor_green/ .vtk 0.02 $PWD/scripts/criticalPoints_TaylorGreen.py > criticalPoints_TaylorGreen_$1_$2_$3.csv
 
 
