e138191c1aae653c0db728414ffd269a74dcc854
Fellegara <Riccardo.Fellegara@dlr.de>
2021-02-18 14:25:05 +0100

    :wrench: Update HPDA scripts

--- scripts/linux/startLorenz_HPDA.sh
+++ scripts/linux/startLorenz_HPDA.sh
@@ -5,8 +5,9 @@ export PV_PLUGIN_PATH=../lib/paraview-5.8/plugins/VestecPlugins
 
 export OMP_NUM_THREADS=$3
 export OMP_DISPLAY_ENV=true
-export OMP_PROC_BIND=spread #close,master,spread
-export OMP_PLACES=cores #socket,cores,threads
+export OMP_PROC_BIND=true #close,master,spread,true,false
+export OMP_PLACES=cores #sockets,cores,threads
+export OMP_SCHEDULE=static #dynamic,static
 
 srun -N $1 -n $2 -c 56 --exclusive ./VestecCatalystEmulator /scratch/VESTEC-DATASETS/lorenz/ .vtk 0.02 $PWD/scripts/criticalPoints_Lorenz.py > criticalPoints_Lorenz_$1_$2_$3.csv
 
